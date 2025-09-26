7f550142a073eb06b17f2b062d37820869d49228
Nick Brandaleone <nbrand@mac.com>
2025-09-25 20:44:45 -0400

    Made service public

--- src/load_generator.cr
+++ src/load_generator.cr
@@ -3,7 +3,8 @@ require "http/client"
 loop do
   sequence = (rand * 41000).to_i
   print "Fibonacci ##{sequence}: "
-  response = HTTP::Client.get("http://127.0.0.1:5000?n=#{sequence}")
+  #response = HTTP::Client.get("http://127.0.0.1:5000?n=#{sequence}")
+  response = HTTP::Client.get("https://otel-cloud-run-demo-161156519703.us-central1.run.app?n=#{sequence}")
   if response.success?
     puts response.body
   else
